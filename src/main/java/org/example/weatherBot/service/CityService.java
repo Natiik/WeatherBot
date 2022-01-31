2bcd4eef46a4821a966903bd376d1d43248241b9
Anastasiia <komaranastasiaa@gmail.com>
2022-01-31 17:52:07 +0200

    code

--- src/main/java/org/example/weatherBot/service/CityService.java
+++ src/main/java/org/example/weatherBot/service/CityService.java
@@ -1,6 +1,5 @@
 package org.example.weatherBot.service;
 
-import org.example.weatherBot.json.JsonMapper;
 import org.example.weatherBot.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +9,6 @@ public class CityService {
 
     @Autowired
     private final CityRepository cityRepository;
-    private JsonMapper jsonMapper = new JsonMapper();
 
     public CityService(CityRepository cityRepository) {
         this.cityRepository = cityRepository;
