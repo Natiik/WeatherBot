277dee47468b9b428822af567e9b6349355b90c8
Anastasiia <komaranastasiaa@gmail.com>
2022-02-01 22:30:41 +0200

    deploy adjust

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 mvn clean package
 ssh root@65.108.88.95 "killall java"
 echo "old bot stopped"
-ssh root@65.108.88.95 "rm WeatherBot-1.0-SNAPSHOT-jar-with-dependencies.jar"
+ssh root@65.108.88.95 "rm WeatherBot.jar"
 echo "old bot removed"
-scp target/WeatherBot-1.0-SNAPSHOT-jar-with-dependencies.jar  root@65.108.88.95:/root/
+scp target/WeatherBot.jar  root@65.108.88.95:/root/
 echo "new bot copied"
-ssh root@65.108.88.95 "java -jar WeatherBot-1.0-SNAPSHOT-jar-with-dependencies.jar" &
+ssh root@65.108.88.95 "java -jar WeatherBot.jar --spring.config.additional-location=/root/lalala.properties" &
 echo "new bot started"
